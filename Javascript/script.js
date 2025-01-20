aeda2cc2e7d7f9226b239d673cc558c2a350b771
Isabela9417 <isabelachana@gmail.com>
2025-01-20 15:18:44 +0200

    updated code

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,65 +1,21 @@
-console.clear();
-
-const elPrevButton = document.querySelector('#prev');
-const elNextButton = document.querySelector('#next');
-const flipping = new Flipping();
-
-const elImages = Array.from(document.querySelectorAll('.ui-big-image'));
-const elThumbnails = Array.from(document.querySelectorAll('.ui-thumbnail'));
-
-let state = {
-  photo: 0
-};
-
-function send(event) {
-  // read cuticle positions
-  flipping.read();
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
   
-  const elActives = document.querySelectorAll('[data-active]');
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
   
-  Array.from(elActives)
-    .forEach(el => el.removeAttribute('data-active'));
-
-  switch (event) {
-    case 'PREV':
-      state.photo--;
-      // Math.max(state.photo - 1, 0);
-      break;
-    case 'NEXT':
-      state.photo++;
-      // Math.min(state.photo + 1, elImages.length - 1);
-      break;
-    default:
-      state.photo = +event;
-      break;
-  }
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   
-  var len = elImages.length;
-  // Loop Around
-  //state.photo = ( ( state.photo % len) + len ) % len;
-  state.photo = Math.max(0, Math.min( state.photo, len - 1) );
-
-  Array.from(document.querySelectorAll(`[data-key="${state.photo}"]`))
-    .forEach( el => {
-    el.setAttribute('data-active', true);
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
-  
-  // execute the FLIP animation
-  flipping.flip();
-}
-
-elThumbnails.forEach( thumb => { 
-  thumb.addEventListener('click', () => {
-    send(thumb.dataset.key);
-  });
-});
-
-elPrevButton.addEventListener('click', () => {
-  send('PREV');
-});
-
-elNextButton.addEventListener('click', () => {
-  send('NEXT');
-});
-
-send(0);
