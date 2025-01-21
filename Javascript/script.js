3dea10117e86fcaecb3acb4048492d29aa05029a
Isabela9417 <isabelachana@gmail.com>
2025-01-21 16:07:27 +0200

    done, needs polishing

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,33 @@
+// home page slider
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("slide");
+
+    
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.opacity = 0;
+    }
+
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1; 
+    }
+
+    
+    slides[slideIndex - 1].style.opacity = 1;
+
+    
+    setTimeout(showSlides, 4000);
+}
+
+showSlides();
+
+
+// the product slider
+
+
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
@@ -19,3 +49,6 @@ const swiper = new Swiper('.swiper-container', {
       el: '.swiper-scrollbar',
     },
   });
+
+
+  
