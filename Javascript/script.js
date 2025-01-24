4259db7ae4cb4a3788ee452b8e5de28a7e9e3c3c
Isabela9417 <isabelachana@gmail.com>
2025-01-24 15:59:20 +0200

    working on the responsive part

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,52 +3,57 @@ let slideIndex = 0;
 
 function showSlides() {
     let slides = document.getElementsByClassName("slide");
-
-    
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.opacity = 0;
     }
 
-    
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1; 
     }
 
-    
     slides[slideIndex - 1].style.opacity = 1;
-
-    
     setTimeout(showSlides, 4000);
 }
-
 showSlides();
 
 
 // the product slider
+// Initialize the Shirts Swiper
+const swiperShirts = new Swiper('.mySwiperShirts', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
+// Initialize the Hoodies Swiper
+const swiperHoodies = new Swiper('.mySwiperHoodies', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
-
-  
+// Initialize the Sweaters Swiper
+const swiperSweaters = new Swiper('.mySwiperSweaters', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
